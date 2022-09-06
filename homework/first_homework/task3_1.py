cf5bba6bced48490b603e645183d0b784540fbf3
oleg <olegarxo15@gmail.com>
2022-09-06 09:48:13 +0300

    Дополнил 3.1

--- homework/first_homework/task3_1.py
+++ homework/first_homework/task3_1.py
@@ -1,16 +1,43 @@
-from random import randint, random
-
-b = int(random() * 30)
+from random import randint
+b = int(randint(1,30))
 a = int(input('Введите четверть'))
 if a == 1:
     print(f'Диапазон чисел X от 0 до {b}')
-    print(f'Диапазон чисел X от 0 до {b}')
+    print('А именоо: ')
+    for i in range(b):
+        print(i, end=' ')
+    print()
+    print(f'Диапазон чисел Y от 0 до {b}')
+    print('А именоо: ')
+    for i in range(b):
+        print(i, end=' ')
 elif a == 2:
     print(f'Диапазон чисел X от 0 до {-b}')
+    print('А именоо: ')
+    for i in range(b,-1,-1):
+        print(-i, end=' ')
+    print()
     print(f'Диапазон чисел Y от 0 до {b}')
+    print('А именоо: ')
+    for i in range(b + 1):
+        print(i, end=' ')
 elif a == 3:
-    print(f'Диапазон чисел X от 0 до {-b}')
-    print(f'Диапазон чисел Y от 0 до {-b}')
+    print(f'Диапазон чисел X от 0 до {b}')
+    print('А именоо: ')
+    for i in range(b,-1,-1):
+        print(-i, end=' ')
+    print()
+    print(f'Диапазон чисел Y от 0 до {b}')
+    print('А именоо: ')
+    for i in range(b,-1,-1):
+        print(-i, end=' ')
 elif a == 4:
     print(f'Диапазон чисел X от 0 до {b}')
-    print(f'Диапазон чисел Y от 0 до {-b}')
+    print('А именоо: ')
+    for i in range(b,-1,-1):
+        print(-i, end=' ')
+    print()
+    print(f'Диапазон чисел Y от 0 до {b}')
+    print('А именоо: ')
+    for i in range(b + 1):
+        print(i, end=' ')
