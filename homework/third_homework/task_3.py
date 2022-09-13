b89e8f280aff81887d9577383d3d8f7231c37410
oleg <olegarxo15@gmail.com>
2022-09-13 09:26:38 +0300

    Исправил ошибку

--- homework/third_homework/task_3.py
+++ homework/third_homework/task_3.py
@@ -8,8 +8,10 @@ for i in range(a):
     b = float(input('Введите число: '))
     lists.append(b)
     list_dec.append(b - int(b))
-for i in range(leng):
-    for g in range(leng - i):
+for i in range(leng - 1):
+    for g in range(leng - i - 1):
         if list_dec[g] > list_dec[g + 1]:
             list_dec[g], list_dec[g + 1] = list_dec[g + 1], list_dec[g]
+print(lists)
+print(list_dec)
 print(f'{lists}->{list_dec[-1] - list_dec[0]}')
