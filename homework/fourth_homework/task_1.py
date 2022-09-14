f4092a7bf9c94d9c3aa5be770e2720259e9e3ce5
oleg <olegarxo15@gmail.com>
2022-09-14 10:59:53 +0300

    Сделал пока через самую простую формулу

--- homework/fourth_homework/task_1.py
+++ homework/fourth_homework/task_1.py
@@ -1,4 +1,7 @@
-# в условии не было сказано, что нельзя брать значение из библиотеки
-from math import pi as p
+p = 3
+for i in range(4, 100):
+    p += 4 / ((i - 2) * (i - 1) * i)
+    p -= 4 / (i * (i + 1) * (i + 2))
+print(p)
 coun = int(input('Введите количество знаков: '))
 print(int(p * (10 ** coun)) / (10 ** coun))
