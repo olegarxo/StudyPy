01a1f712e91c694d1aac26891e5178c905b75a31
oleg <olegarxo15@gmail.com>
2022-09-15 14:25:07 +0300

    Работают функции, осталось объеденить в формулу

--- homework/fourth_homework/task_5.py
+++ homework/fourth_homework/task_5.py
@@ -13,28 +13,44 @@ def Dict_numdeg_val(sum_list):   #
     for i in sum_list:
         j = - 1
         k = 0
+        flag = True
         count = 0
         tamp_deg = 0
         tamp_num = 0
         leng = len(i)
         while (flag):
-            if i[j] == 'x':
+            if i[-1] == 'x':
+               tamp_deg = 1
+               degree_val[tamp_deg] = 0
+               flag = False
+               count = j
+            elif fab(j) == leng + 1:
+                degree_val[0] = tamp_deg
+                flag = False
+                count = j
+                return degree_val
+            elif i[j] == 'x':
                 degree_val[tamp_deg] = 0
                 flag = False
                 count = j
             else:
-                tamp_deg += int(i[j]) * (10 ** (fab(j) - 1))
+                tamp_deg += int(int(i[j]) * (10 ** (fab(j) - 1)))
                 j -= 1
         flag = True
-        count_d = 10 * (fab(count) - (k + 1))
         while (flag):
             if (leng + count) == 0:
                 flag = False
             else:
-                tamp_num += int((i[k]) * int((count_d / 10)))
+                tamp_num += int(i[k]) * (10**(leng + (count - 1)))
+                degree_val[tamp_deg] = tamp_num
             k += 1
             count -= 1
     return degree_val
+def Sum_dict(item_one, item_two):
+    for key, value in item_one.items():
+        item_one[key] = value + item_two.get(key, 0)
+    return item_one
+
 open_file1 = 'f1.txt' #input(('Вставьте имя первого файла из папки : '))
 open_file2 = 'f2.txt'   #input(('Вставьте имя первого файла из папки : '))
 text_one = Read_file(open_file1)
@@ -45,27 +61,13 @@ new_text_one = ListNum(text_one)
 new_text_two = ListNum(text_two)
 print(new_text_one)
 print(new_text_two)
-new_text_dic = Dict_numdeg_val(new_text_one)
-print(new_text_dic)
+new_dic_one = Dict_numdeg_val(new_text_one)
+new_dyc_two = Dict_numdeg_val(new_text_two)
+print(new_dic_one)
+print(new_dyc_two)
+if len(new_text_two) > len(new_text_one):
+    tamp = Sum_dict(new_dyc_two, new_dic_one)
+else:
+    tamp = Sum_dict(new_dic_one, new_dyc_two)
+print(tamp)
 exit()
-text_one = ListNum(Read_file(open_file1))
-text_two = ListNum(Read_file(open_file2))
-first_string_new = []
-print(first_string)
-for i in first_string:
-    print(i)
-    if i == '=' or 'x' or '^':
-        first_string_new.append('')
-    else:
-        first_string_new.append(i)
-print(first_string_new)
-print(list_str)
-tamp = 0
-i = 0
-while flag:
-    if list_str[i] == '^' or ' ':
-        list_str.pop(i)
-    elif list_str == '=':
-        flag = False
-    i += 1
-print(list_str)
