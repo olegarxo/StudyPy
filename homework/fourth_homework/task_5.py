a0582e83ba1360e2d620e6fd495077b6684af866
oleg <olegarxo15@gmail.com>
2022-09-15 10:48:27 +0300

    В процессе написания функции для забора степени и коэффициента Х

--- homework/fourth_homework/task_5.py
+++ homework/fourth_homework/task_5.py
@@ -1,19 +1,41 @@
+from math import fabs as fab
 def ListNum(string):
     string = string.replace(' ', '').replace('^', '').split('=')
     left_string = string[0].split('+')
     return left_string
 def Read_file(open_file):
-    f = open(open_file, 'r+')
-    text: str = f.readline()
-    open_file.close()
+    with open(open_file, 'r+') as f:
+        text = f.readline()
     return text
-open_file1 = open(input('Вставьте имя первого файла из папки : '), 'w+')
-open_file2 = open(input('Вставьте имя первого файла из папки : '), 'w+')
+def Dict_numdeg_val(sum_list):
+    degree_val = { }
+    flag = True
+    for i in sum_list:
+        j = - 1
+        k = 0
+        tamp_deg = 0
+        tamp_num = 0
+        leng = len(i)
+        while (flag):
+            tamp_deg += int(i[j]) * (10 * fab(j))
+            if i[j] == 'x':
+                degree_val[tamp_deg] = 0
+                flag = False
+        while (flag):
+            tamp_num += int(i[k]) * (10 * fab(j))
+
+
+
+open_file1 = input(('Вставьте имя первого файла из папки : '))
+open_file2 = input(('Вставьте имя первого файла из папки : '))
 text_one = Read_file(open_file1)
 text_two = Read_file(open_file2)
 print(text_one)
 print(text_two)
-
+new_text_one = ListNum(text_one)
+new_text_two = ListNum(text_two)
+print(new_text_one)
+print(new_text_two)
 exit()
 text_one = ListNum(Read_file(open_file1))
 text_two = ListNum(Read_file(open_file2))
