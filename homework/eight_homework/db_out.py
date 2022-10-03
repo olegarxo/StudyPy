d1e4b4b01a07b3200faa50bd667036fad21a3cf7
oleg <olegarxo15@gmail.com>
2022-10-03 10:32:27 +0300

    Сделал взаимодействие базой данных

---
+++ homework/eight_homework/db_out.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+con = sqlite3.connect('school.db')
+
+
+def Out_schedule():
+    cursorObj = con.cursor()
+    cursorObj.execute('SELECT * FROM schedule')
+    [print(*row, sep='\t', end='\t') for row in cursorObj.fetchall()]
+
+
+def Out_students():
+    cursorObj = con.cursor()
+    cursorObj.execute('SELECT * FROM students')
+    [print(*row, sep='\t', end='\t') for row in cursorObj.fetchall()]
+
+
+def Out_teachers():
+    cursorObj = con.cursor()
+    cursorObj.execute('SELECT * FROM teachers')
+    [print(*row, sep='\t', end='\t') for row in cursorObj.fetchall()]
+
+
+Out_teachers()
