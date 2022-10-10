ce7541f61709e51ab7173ebdd7063b4fdf45e080
oleg <olegarxo15@gmail.com>
2022-10-10 14:43:32 +0300

    Доделал счет

--- homework/nine_homework/comand.py
+++ homework/nine_homework/comand.py
@@ -1,10 +1,14 @@
-from telegram import Bot
+from turtle import update
+from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from random import randint as rd
 
 
 def som_comand(update, context):
     context.bot.send_message(update.effective_chat.id, 'Я таких команд не знаю')
+
+
+
 
 
 def take_text(update, context):
@@ -21,13 +25,22 @@ def take_text(update, context):
                 text[tamp] = ''
     text = list(filter(lambda a: a != '' and a != '/del', text))
     context.bot.send_message(update.effective_chat.id, ' '.join(text))
+
+
 A = 0
 B = 1
 C = 2
 global snack
 snack = 100
-def start_game(update, context):
+global count_win_bot
+global count_win_user 
+count_win_bot = 0
+count_win_user = 0
 
+
+def start_game(update, context):
+    global snack
+    snack = 100
     context.bot.send_message(update.effective_chat.id, 'Привет, ну что ж, сыграем)')
     random_number = rd(0, 1)
     if random_number == 0:
@@ -40,22 +53,27 @@ def start_game(update, context):
 
 def bot_game(update, context):
     global snack
+    global count_win_user 
+    global count_win_bot
     if snack // 28 > 1:
         take = rd(10, 29)
     elif 0 < snack < 29:
         take = snack
-        context.bot.send_message(update.effective_chat.id, 'Победил БОТ ')
+        count_win_bot += 1
+        context.bot.send_message(update.effective_chat.id, f'Победил БОТ\nОчки бота:{count_win_bot}\n{update.effective_user.first_name}, вот твои очки:{count_win_user}')
         return ConversationHandler.END
     elif snack < 100:
         take = rd(5, 12)
     else:
         take = snack % 28
     context.bot.send_message(update.effective_chat.id, f'Я беру {take} конфет\nОсталось {snack - take})')
+    snack -= take
     return A
-
 
 def gamer(update, context):
     global snack
+    global count_win_user 
+    global count_win_bot
     flag = True
     while flag:
         text = update.message.text
@@ -63,10 +81,11 @@ def gamer(update, context):
             snack -= int(text)
             context.bot.send_message(update.effective_chat.id, f'Осталось {snack} конфет')
             flag = False
-        else: #Впадает в цикл
+        else:  # Впадает в цикл
             context.bot.send_message(update.effective_chat.id, 'Неправильное число, попробуйте еще раз')
     if snack == 0:
-        context.bot.send_message(update.effective_chat.id, 'Победил ТЫ ')
+        count_win_user += 1
+        context.bot.send_message(update.effective_chat.id, f'Победил ТЫ\nОчки бота:{count_win_bot}\n{update.effective_user.first_name}, вот твои очки:{count_win_user}')
         return ConversationHandler.END
     return B
 
@@ -75,3 +94,9 @@ def end_game(update, context):
     global snack
     context.bot.send_message(update.effective_chat.id, 'Победил ')
     snack = 300
+def Show_point(update, context):
+    context.bot.send_message(update.effective_chat.id, f'Очки бота:{count_win_bot}\nТвои очки:{count_win_user}')
+def Null_point(update, context):
+    global count_win_bot
+    global count_win_user 
+    (count_win_bot, count_win_user) = (0, 0) 
--- homework/nine_homework/main.py
+++ homework/nine_homework/main.py
@@ -1,8 +1,7 @@
-from telegram import Bot
+from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from random import randint as rd
 from comand import *
-
 bot = Bot(token='5767488864:AAGCvEygDRnhXvfOs9Rf8NAndTxArryjgo4')
 updater = Updater(token='5767488864:AAGCvEygDRnhXvfOs9Rf8NAndTxArryjgo4')
 dispatcher = updater.dispatcher
@@ -11,18 +10,24 @@ dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start_game)
 del_abv = CommandHandler('del', take_text)   # /del (Удаляет слова, в которых есть буквы из списка)
-bot_move = MessageHandler(Filters.text, bot_game)
-gamer_move = MessageHandler(Filters.text, gamer)
-end = CommandHandler('cancel', end_game)
+null = CommandHandler('null', Null_point)
+points = CommandHandler('point', Show_point)
+bot_move = MessageHandler(Filters.all, bot_game)
+gamer_move = MessageHandler(Filters.all, gamer)
+end = MessageHandler(Filters.all, end_game)
 Game = ConversationHandler(entry_points=[start_handler],
                            states={A: [gamer_move], B: [bot_move]},
                            fallbacks=[end])
+                         
 
 # dispatcher.add_handler(start_handler)s
 dispatcher.add_handler(del_abv)
 # dispatcher.add_handler(bot_move)
 # dispatcher.add_handler(gamer_move)
 dispatcher.add_handler(Game)
+dispatcher.add_handler(null)
+dispatcher.add_handler(points)
+dispatcher.add_handler(end)
 
 updater.start_polling()
 updater.idle()
